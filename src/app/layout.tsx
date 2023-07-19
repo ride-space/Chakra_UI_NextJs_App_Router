fab4e386bb6beaf0f3c1cde2684a636dc8f4c587
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-07-20 01:01:41 +0900

    モーダルとトースターの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { NavigationMemo } from "@/app/_components";
+import {
+  NavigationMemo,
+  SignUpModal,
+  LoginModal,
+  ProfileModal,
+} from "@/app/_components";
+import getCurrentUser from "@/app/_action/getCurrentUser";
+import { AuthContext, ToasterContext } from "@/app/_context";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,24 +20,31 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html>
       <body className={inter.className}>
-        <div>
-          <div className="flex min-h-screen flex-col">
-            <NavigationMemo />
+        <AuthContext>
+          <ToasterContext />
+          <SignUpModal />
+          <LoginModal />
+          <ProfileModal currentUser={currentUser} />
+          <div>
+            <div className="flex min-h-screen flex-col">
+              <NavigationMemo currentUser={currentUser} />
 
-            <main className="container mx-auto max-w-screen-sm flex-1 px-1 py-5">
-              {children}
-            </main>
+              <main className="container mx-auto max-w-screen-sm flex-1 px-1 py-5">
+                {children}
+              </main>
 
-            <footer className="py-5">
-              <div className="text-center text-sm">
-                Copyright © All rights reserved | Prisma Auth
-              </div>
-            </footer>
+              <footer className="py-5">
+                <div className="text-center text-sm">
+                  Copyright © All rights reserved | Prisma Auth
+                </div>
+              </footer>
+            </div>
           </div>
-        </div>
+        </AuthContext>
       </body>
     </html>
   );
