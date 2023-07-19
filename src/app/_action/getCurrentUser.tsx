e04d930173b273daf25f3ed87016c7d3b6c22a2b
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-07-20 01:03:39 +0900

    user情報の取得

---
+++ src/app/_action/getCurrentUser.tsx
@@ -0,0 +1,27 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+const getCurrentUser = async () => {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const response = await prisma?.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!response) {
+      return null;
+    }
+
+    return response;
+  } catch (error) {
+    return null;
+  }
+};
+
+export default getCurrentUser;
