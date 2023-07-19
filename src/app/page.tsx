89c870258ad1cff5544ebd00a7ebd3a2c0c84755
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-07-20 01:03:14 +0900

    認証が分かるように表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-const Home = () => {
+import getCurrentUser from "./_action/getCurrentUser";
+
+const Home = async () => {
+  const currentUser = await getCurrentUser();
   return (
     <div>
-      <div>Main</div>
+      <div className="text-center">{currentUser ? <div>認証中</div> : <div>未認証</div>}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
